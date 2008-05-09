514c1dc8e78bbd45f37c29efb088b5b759ecd965
David McKain <david.mckain@ed.ac.uk>
2008-05-09 15:46:58 +0000

    This commit fills in the blanks for the demo/documentation webapp, which is now just about taking some kind of shape.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@29 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

---
+++ demowebapp/WEB-INF/webpage.xsl
@@ -0,0 +1,99 @@
+<?xml version="1.0"?>
+<!--
+
+$Id: header.jspf 2712 2008-03-10 17:01:01Z davemckain $
+
+Stylesheet to apply UoE headers and footers to SnuggleTeX web page output
+
+Copyright (c) 2008 University of Edinburgh.
+All Rights Reserved
+
+-->
+<xsl:stylesheet version="1.0"
+  xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+  xmlns:html="http://www.w3.org/1999/xhtml"
+  xmlns="http://www.w3.org/1999/xhtml"
+  exclude-result-prefixes="html">
+
+  <!-- Need to pass webapp context path so as to locate images and stuff -->
+  <xsl:param name="context-path"/>
+
+  <xsl:template match="html:*">
+    <xsl:copy>
+      <xsl:copy-of select="@*"/>
+      <xsl:apply-templates/>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template match="html:body">
+    <body>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" id="header">
+        <tr>
+          <td width="84" align="left" valign="top">
+            <a href="http://www.ed.ac.uk" class="headertext"><img
+              src="{$context-path}/includes/uoe_logo.jpg"
+              alt="The University of Edinburgh" id="logo" name="logo"
+              width="84" height="84" border="0" /></a>
+          </td>
+          <td align="left">
+            <h3>
+              THE UNIVERSITY of EDINBURGH
+            </h3>
+            <h1>
+              SCHOOL OF PHYSICS
+            </h1>
+          </td>
+          <td align="right">
+            <table border="0" cellpadding="0" cellspacing="0" style="margin-right:20px;">
+              <tr>
+                <td>
+                  <a href="http://www.ed.ac.uk" class="headertext"><img
+                    src="{$context-path}/includes/arrow.gif"
+                    width="16" height="11" border="0"
+                    alt="" />University&#160;Homepage</a>
+                  <br />
+                  <a href="http://www.ph.ed.ac.uk" class="headertext"><img
+                    src="{$context-path}/includes/arrow.gif"
+                    width="16" height="11" border="0"
+                    alt="" />School of Physics Homepage</a>
+                  <br />
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <h1 id="location">
+        <a href="{$context-path}">
+          SnuggleTeX (v@uk.ac.ed.ph.snuggletex.version@)
+        </a>
+      </h1>
+      <div id="content">
+        <xsl:copy-of select="node()"/>
+      </div>
+      <div id="copyright">
+        SnuggleTeX Release @uk.ac.ed.ph.snuggletex.version@
+        <br />
+        Copyright &#xa9; 2008
+        <a href="http://www.ph.ed.ac.uk">The School of Physics</a>,
+        <a href="http://www.ed.ac.uk">The University of Edinburgh</a>.
+        <br />
+        For more information, contact
+        <a href="http://www.ph.ed.ac.uk/elearning/contacts/#dmckain">David McKain</a>.
+      </div>
+    </body>
+  </xsl:template>
+
+  <xsl:template match="html:h1">
+    <h2>
+      <xsl:apply-templates/>
+    </h2>
+  </xsl:template>
+
+  <xsl:template match="html:h2">
+    <h3>
+      <xsl:apply-templates/>
+    </h3>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ demowebapp/java/uk/ac/ed/ph/snuggletex/demowebapp/SnuggleTeXServlet.java
@@ -0,0 +1,244 @@
+/* $Id$
+ *
+ * Copyright 2008 University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.snuggletex.demowebapp;
+
+import uk.ac.ed.ph.aardvark.commons.util.IOUtilities;
+import uk.ac.ed.ph.snuggletex.SessionConfiguration;
+import uk.ac.ed.ph.snuggletex.Snapshot;
+import uk.ac.ed.ph.snuggletex.SnuggleInput;
+import uk.ac.ed.ph.snuggletex.SnuggleTeXEngine;
+import uk.ac.ed.ph.snuggletex.SnuggleTeXSession;
+import uk.ac.ed.ph.snuggletex.WebPageBuilderOptions;
+import uk.ac.ed.ph.snuggletex.DOMBuilderOptions.ErrorOutputOptions;
+import uk.ac.ed.ph.snuggletex.WebPageBuilderOptions.WebPageType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.transform.Templates;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamSource;
+
+/**
+ * Trivial servlet used to serve up the sample SnuggleTeX pages within the documentation
+ * webapp.
+ * <p>
+ * This handles URLs of the form: <tt>/contextPath/content/pageName/file.ext</tt>
+ * <p>
+ * The <tt>pageName</tt> is mapped to a file <tt>pageName.tex</tt> under {@link #MACROS_RESOURCE_PATH}
+ * and the <tt>type.ext</tt> is used to determine which type of web page to produce.
+ *
+ * @author  David McKain
+ * @version $Revision$
+ */
+public final class SnuggleTeXServlet extends HttpServlet {
+    
+    private static final long serialVersionUID = 7013613625143346274L;
+    
+    /** Directory in which SnuggleTeX pages will be loaded from (relative to webapp)*/
+    public static final String CONTENT_BASE_DIRECTORY =  "/WEB-INF/content/";
+    
+    /** Location of the global LaTeX macros file (relative to webapp) */
+    public static final String MACROS_RESOURCE_PATH = "/WEB-INF/macros.tex";
+    
+    /** Location of the list of content pages (which maps each name to its title) */
+    public static final String PAGES_RESOURCE_PATH = "/WEB-INF/pages.properties";
+    
+    /** Location of XSLT applied to resulting page to add in headers and footers */
+    public static final String WEBPAGE_XSLT_RESOURCE_PATH = "/WEB-INF/webpage.xsl";
+    
+    /** 
+     * The XSLT {@link Templates} Object created from {@link #WEBPAGE_XSLT_RESOURCE_PATH}
+     * will be saved into the {@link ServletContext} as an attribute having this name.
+     */
+    public static final String WEBPAGE_XSLT_ATTRIBUTE_NAME = "webpageXslt";
+    
+    /**
+     * Special <tt>file.ext</tt> used to request the original LaTeX source of a page, rather
+     * than a finished web page.
+     */
+    public static final String SOURCE_TYPE_EXT = "source.txt";
+    
+    /** Maps <tt>file.ext</tt> to a {@link WebPageType} (set up in {@link #init()}) */
+    private Map<String, WebPageType> webPageTypeMap = new HashMap<String, WebPageType>();
+    
+    /** "Map" of all supported content pages (name -> title) */
+    private Properties pagesProperties;
+    private Snapshot snapshot;
+    private Templates webPageTemplates;
+    
+    /**
+     * Helper that reads in a resource from the webapp hierarchy, throwing a {@link ServletException}
+     * if the resource could not be found.
+     * 
+     * @param resourcePath path of Resource to load, relative to base of webapp.
+     * @return resulting {@link InputStream}, which will not be null
+     * @throws ServletException
+     */
+    protected InputStream ensureReadResource(String resourcePath) throws ServletException {
+        InputStream result = getServletContext().getResourceAsStream(resourcePath);
+        if (result==null) {
+            throw new ServletException("Could not read in required web resource at " + resourcePath);
+        }
+        return result;
+    }
+    
+    /**
+     * Uses SnuggleTeX to parse the {@link #MACROS_RESOURCE_PATH} and create a {@link Snapshot}
+     * of the results. This can be reused for every web request, which saves having to re-parse
+     * the macros each time.
+     * 
+     * @throws ServletException
+     */
+    private Snapshot createPostMacrosSnapshot() throws ServletException {
+        /* Read in common macros */
+        InputStream macrosStream = ensureReadResource(MACROS_RESOURCE_PATH);
+        
+        /* Create engine, read in macros and then create a snapshot to reuse for each request */
+        SessionConfiguration configuration = new SessionConfiguration();
+        configuration.setInferringMathStructure(true);
+        
+        SnuggleTeXEngine engine = new SnuggleTeXEngine();
+        SnuggleTeXSession session = engine.createSession(configuration);
+        try {
+            session.parseInput(new SnuggleInput(macrosStream));
+        }
+        catch (IOException e) {
+            throw new ServletException("IOException whilst reading in macros file at " + MACROS_RESOURCE_PATH, e);
+        }
+        return session.createSnapshot();
+    }
+    
+    /**
+     * Initialises certain things that won't change over the lifetime of the web application, such
+     * as the XSLT for generating web pages.
+     * <p>
+     * This also creates a SnuggleTeX {@link Snapshot} Object from the {@link #MACROS_RESOURCE_PATH}
+     * file, which is a useful example of how this can be used.
+     */
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        webPageTypeMap = new HashMap<String, WebPageType>();
+        webPageTypeMap.put("default.xml", WebPageType.DEFAULT);
+        webPageTypeMap.put("crossbrowser.xml", WebPageType.CROSS_BROWSER_XHTML);
+        webPageTypeMap.put("mathplayer.html", WebPageType.MATHPLAYER_HTML);
+        
+        /* Read in list of "content pages" */
+        InputStream propertiesStream = ensureReadResource(PAGES_RESOURCE_PATH);
+        pagesProperties = new Properties();
+        try {
+            pagesProperties.load(propertiesStream);
+        }
+        catch (IOException e) {
+            throw new ServletException("IOException occurred when reading in " + PAGES_RESOURCE_PATH, e);
+        }
+
+        /* Parse common macros and create a snapshot to reuse on each request */
+        this.snapshot = createPostMacrosSnapshot();
+        
+        /* Read in the XSLT stylesheet that will be used to make the resulting web page pretty.
+         * We also save this into the ServletContext as the Static XSLT Servlet will use it as well.
+         */
+        StreamSource tidyupSource = new StreamSource(ensureReadResource(WEBPAGE_XSLT_RESOURCE_PATH));
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        try {
+            webPageTemplates = transformerFactory.newTemplates(tidyupSource);
+        }
+        catch (TransformerConfigurationException e) {
+            throw new ServletException("Could not compiles stylesheet at " + WEBPAGE_XSLT_RESOURCE_PATH);
+        }
+        getServletContext().setAttribute(WEBPAGE_XSLT_ATTRIBUTE_NAME, webPageTemplates);
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String pathInfo = request.getPathInfo(); /* This is "/pageName/type.ext" */
+        String[] bits = pathInfo.substring(1).split("/", 2); /* Should give { "pathName", "type.ext" } */
+        if (bits.length!=2) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "PathInfo not of required form");
+            return;
+        }
+        String pageName = bits[0];
+        String typeExt = bits[1];
+        
+        /* Check that the page requested is listed */
+        String pageTitle = pagesProperties.getProperty(pageName);
+        if (pageTitle==null) {
+            getServletContext().log("Page " + pageName + " is not listed in " + PAGES_RESOURCE_PATH);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        
+        /* Work out where the source TeX is coming from and read it in, if found */
+        String contentResourceName = CONTENT_BASE_DIRECTORY + pageName + ".tex";
+        InputStream contentStream = getServletContext().getResourceAsStream(contentResourceName);
+        if (contentStream==null) {
+            getServletContext().log("Could not read in content stream " + contentResourceName);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        
+        /* Handle request for page source now, if appropriate */
+        if (typeExt.equals(SOURCE_TYPE_EXT)) {
+            response.setContentType("text/plain");
+            IOUtilities.transfer(contentStream, response.getOutputStream());
+            return;
+        }
+
+        /* Decide what type of page we're making */
+        WebPageType pageType = webPageTypeMap.get(typeExt);
+        if (pageType==null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Could not deduce type of resulting page");
+            return;
+        }
+        
+        /* Parse the TeX */
+        SnuggleTeXSession session = snapshot.createSession();
+        
+        SnuggleInput input = new SnuggleInput(contentStream, contentResourceName);
+        session.parseInput(input);
+        
+        /* Create stylesheet to prettify the resulting web page */
+        Transformer stylesheet;
+        try {
+            stylesheet = webPageTemplates.newTransformer();
+            stylesheet.setParameter("context-path", request.getContextPath());
+        }
+        catch (TransformerConfigurationException e) {
+            throw new ServletException("Could not create stylesheet from Templates", e);
+        }
+        
+        /* Generate and serve the resulting web page */
+        WebPageBuilderOptions options = new WebPageBuilderOptions();
+        options.setPageType(pageType);
+        options.setStylesheet(stylesheet);
+        options.setErrorOptions(ErrorOutputOptions.XHTML);
+        options.setTitle(pageTitle);
+        options.setAddingTitleHeading(true);
+        options.setCSSStylesheetURLs(
+                request.getContextPath() + "/includes/physics.css",
+                request.getContextPath() + "/includes/snuggletex.css"
+        );
+        try {
+            session.writeWebPage(options, response, response.getOutputStream());
+        }
+        catch (Exception e) {
+            throw new ServletException("Unexpected Exception", e);
+        }
+    }
+}
