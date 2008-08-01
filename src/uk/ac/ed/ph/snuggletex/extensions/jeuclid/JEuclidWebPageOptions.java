0b78f562356315d35c8359bb25f79abac971e088
David McKain <david.mckain@ed.ac.uk>
2008-08-01 13:00:24 +0000

    Removed the outputtingXHML property - forgot to do this when I introduced the more general SerializationMethod property.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@175 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

--- src/uk/ac/ed/ph/snuggletex/extensions/jeuclid/JEuclidWebPageOptions.java
+++ src/uk/ac/ed/ph/snuggletex/extensions/jeuclid/JEuclidWebPageOptions.java
@@ -18,19 +18,9 @@ public final class JEuclidWebPageOptions extends AbstractWebPageOptions {
     /** Callback to use when saving out any images produced by the process. */
     private MathMLImageSavingCallback imageSavingCallback;
     
-    /**
-     * Flag to set to output (backward-compatible) XHTML in preference to HTML.
-     * <p>
-     * (This only has an effect if you are using an XSLT 2.0 processor since the "xhtml" will
-     * be used in this case; otherwise trying to output properly backward-compatible XHTML
-     * using a XSLT 1.0 is not worth trying.)
-     */
-    private boolean outputtingXHTML;
-    
     public JEuclidWebPageOptions() {
         super();
         this.imageSavingCallback = null;
-        this.outputtingXHTML = false;
     }
     
     public MathMLImageSavingCallback getImageSavingCallback() {
@@ -39,14 +29,5 @@ public final class JEuclidWebPageOptions extends AbstractWebPageOptions {
 
     public void setImageSavingCallback(MathMLImageSavingCallback callback) {
         this.imageSavingCallback = callback;
-    }
-
-    
-    public boolean isOutputtingXHTML() {
-        return outputtingXHTML;
-    }
-    
-    public void setOutputtingXHTML(boolean outputtingXHTML) {
-        this.outputtingXHTML = outputtingXHTML;
     }
 }
