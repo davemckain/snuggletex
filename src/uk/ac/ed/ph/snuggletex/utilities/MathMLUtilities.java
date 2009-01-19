b2086ac20042bc72c6125993d9ed0e481b8dd4a8
David McKain <david.mckain@ed.ac.uk>
2009-01-19 22:02:53 +0000

    Updated methods in MathMLUtilities for extracting annotations to make them more amenable to pulling out all annoations via a simple result Object.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@246 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

--- src/uk/ac/ed/ph/snuggletex/utilities/MathMLUtilities.java
+++ src/uk/ac/ed/ph/snuggletex/utilities/MathMLUtilities.java
@@ -6,11 +6,11 @@
 package uk.ac.ed.ph.snuggletex.utilities;
 
 import static org.w3c.dom.Node.ELEMENT_NODE;
-import static uk.ac.ed.ph.snuggletex.definitions.Globals.MATHML_NAMESPACE;
 
 import uk.ac.ed.ph.commons.util.ConstraintUtilities;
 import uk.ac.ed.ph.commons.util.StringUtilities;
 import uk.ac.ed.ph.snuggletex.SnuggleRuntimeException;
+import uk.ac.ed.ph.snuggletex.definitions.Globals;
 import uk.ac.ed.ph.snuggletex.internal.XMLUtilities;
 
 import java.io.IOException;
@@ -37,11 +37,14 @@ import org.xml.sax.SAXException;
  */
 public final class MathMLUtilities {
     
+    public static final String ANNOTATION_LOCAL_NAME = "annotation";
+    public static final String ANNOTATION_XML_LOCAL_NAME = "annotation-xml";
+    
     /**
      * Convenience method that parses a MathML document specified as a UTF-8-encoded String.
      * 
-     * @param mathmlDocument
      * @return DOM Document
+     * @param mathmlDocument
      * @throws IOException
      * @throws SAXException
      */
@@ -127,6 +130,55 @@ public final class MathMLUtilities {
     //---------------------------------------------------------------------
 
     /**
+     * Convenience method to unwrap at MathML DOM Object containing top-level parallel markup,
+     * as defined Section 5.3.1 of the MathML 2.0 specification.
+     * <p>
+     * If there is no parallel markup detected then null if returned.
+     * <p>
+     * If the given element is null or is not a MathML "math" element, then an {@link IllegalArgumentException}
+     * is thrown.
+     */
+    public static UnwrappedParallelMathMLDOM unwrapParallelMathMLDOM(Element mathmlElement) {
+        ensureMathMLContainer(mathmlElement);
+        
+        /* Look for semantics child then annotation child with encoding set appropriately */
+        Node search = mathmlElement.getFirstChild();
+        if (!(search.getNodeType()==ELEMENT_NODE && Globals.MATHML_NAMESPACE.equals(search.getNamespaceURI())
+                && "semantics".equals(search.getLocalName()))) {
+            /* Didn't get <semantics/> as first and only child so not parallel markup */
+            return null;
+        }
+        
+        /* OK, this looks like parallel markup */
+        UnwrappedParallelMathMLDOM result = new UnwrappedParallelMathMLDOM();
+        Element semantics = (Element) search;
+        NodeList childNodes = semantics.getChildNodes();
+        
+        /* Pull out the first child */
+        result.setFirstBranch((Element) childNodes.item(0));
+        
+        /* Then pull out annotations, which must be the subsequent children */
+        Element searchElement;
+        for (int i=1, length=childNodes.getLength(); i<length; i++) {
+            search = childNodes.item(i);
+            if (search.getNodeType()==ELEMENT_NODE && Globals.MATHML_NAMESPACE.equals(search.getNamespaceURI())) {
+                searchElement = (Element) search;
+                if (ANNOTATION_LOCAL_NAME.equals(search.getLocalName())) {
+                    result.getTextAnnotations().put(searchElement.getAttribute("encoding"), extractTextElementValue(searchElement));
+                }
+                else if (ANNOTATION_XML_LOCAL_NAME.equals(search.getLocalName())) {
+                    result.getXmlAnnotaions().put(searchElement.getAttribute("encoding"), searchElement.getChildNodes());
+                }
+                else {
+                    /* (Just silently ignore this) */
+                }
+            }
+        }
+        return result;
+    }
+
+
+    /**
      * Extracts the first textual annotation found having the given encoding attribute from
      * the given MathML <tt>math</tt> element, if such an annotation is found.
      * <p>
@@ -144,25 +196,8 @@ public final class MathMLUtilities {
      * @return first matching annotation, or null if not present.
      */
     public static String extractAnnotationString(Element mathmlElement, String encodingAttribute) {
-        Element annotationElement = extractAnnotationElement(mathmlElement, "annotation", encodingAttribute);
-        if (annotationElement==null) {
-            return null;
-        }
-        NodeList childNodes = annotationElement.getChildNodes();
-        String result;
-        if (childNodes.getLength()==1) {
-            /* Text Nodes have been coalesced */
-            result = childNodes.item(0).getNodeValue();
-        }
-        else {
-            /* Need to coalesce manually */
-            StringBuilder resultBuilder = new StringBuilder();
-            for (int i=0; i<childNodes.getLength(); i++) {
-                resultBuilder.append(childNodes.item(i).getNodeValue());
-            }
-            result = resultBuilder.toString();
-        }
-        return result;
+        Element annotationElement = extractAnnotationElement(mathmlElement, ANNOTATION_LOCAL_NAME, encodingAttribute);
+        return annotationElement!=null ? extractTextElementValue(annotationElement) : null;
     }
 
     /**
@@ -180,39 +215,63 @@ public final class MathMLUtilities {
      * ]]></pre>
      * 
      * @param mathmlElement
-     * @return DOM NodeList corresponding to the first matching annotation, or null if not found.
+     * @return DOM NodeList corresponding to the first matching annotation, or null if no such annotation found.
      */
     public static NodeList extractAnnotationXML(Element mathmlElement, String encodingAttribute) {
-        Element annotationElement = extractAnnotationElement(mathmlElement, "annotation-xml", encodingAttribute);
+        Element annotationElement = extractAnnotationElement(mathmlElement, ANNOTATION_XML_LOCAL_NAME, encodingAttribute);
         return annotationElement!=null ? annotationElement.getChildNodes() : null;
     }
 
     private static Element extractAnnotationElement(Element mathmlElement, String annotationElementLocalName, String encodingAttribute) {
-        ConstraintUtilities.ensureNotNull(mathmlElement, "MathML element");
+        ensureMathMLContainer(mathmlElement);
         ConstraintUtilities.ensureNotNull(encodingAttribute, "encoding");
         
         /* (We could have used the XPath API for this but it's almost as easy to traverse the DOM
          * directly here.)
          */
-        if (MATHML_NAMESPACE.equals(mathmlElement.getNamespaceURI()) && "math".equals(mathmlElement.getLocalName())) {
-            /* Look for semantics child then annotation child with encoding set appropriately */
-            Node search = mathmlElement.getFirstChild();
-            if (!(search.getNodeType()==ELEMENT_NODE && MATHML_NAMESPACE.equals(search.getNamespaceURI())
-                    && "semantics".equals(search.getLocalName()))) {
-                /* Didn't get <semantics/> as first and only child */
-                return null;
-            }
-            Element semantics = (Element) search;
-            NodeList childNodes = semantics.getChildNodes();
-            for (int i=0, length=childNodes.getLength(); i<length; i++) {
-                search = childNodes.item(i);
-                if (search.getNodeType()==ELEMENT_NODE && MATHML_NAMESPACE.equals(search.getNamespaceURI())
-                        && annotationElementLocalName.equals(search.getLocalName())
-                        && encodingAttribute.equals(((Element) search).getAttribute("encoding"))) {
-                    return (Element) search;
-                }
+        /* Look for semantics child then annotation child with encoding set appropriately */
+        Node search = mathmlElement.getFirstChild();
+        if (!(search.getNodeType()==ELEMENT_NODE && Globals.MATHML_NAMESPACE.equals(search.getNamespaceURI())
+                && "semantics".equals(search.getLocalName()))) {
+            /* Didn't get <semantics/> as first and only child */
+            return null;
+        }
+        Element semantics = (Element) search;
+        NodeList childNodes = semantics.getChildNodes();
+        for (int i=0, length=childNodes.getLength(); i<length; i++) {
+            search = childNodes.item(i);
+            if (search.getNodeType()==ELEMENT_NODE && Globals.MATHML_NAMESPACE.equals(search.getNamespaceURI())
+                    && annotationElementLocalName.equals(search.getLocalName())
+                    && encodingAttribute.equals(((Element) search).getAttribute("encoding"))) {
+                return (Element) search;
             }
         }
         return null;
     }
+    
+    private static void ensureMathMLContainer(Element mathmlElement) {
+        ConstraintUtilities.ensureNotNull(mathmlElement, "MathML element");
+        if (!(Globals.MATHML_NAMESPACE.equals(mathmlElement.getNamespaceURI()) && "math".equals(mathmlElement.getLocalName()))) {
+            throw new IllegalArgumentException("Not a MathML <math/> element");
+        }
+    }
+    
+    private static String extractTextElementValue(Element textElement) {
+        NodeList childNodes = textElement.getChildNodes();
+        String result;
+        if (childNodes.getLength()==1) {
+            /* Text Nodes have been coalesced */
+            result = childNodes.item(0).getNodeValue();
+        }
+        else {
+            /* Need to coalesce manually */
+            StringBuilder resultBuilder = new StringBuilder();
+            for (int i=0; i<childNodes.getLength(); i++) {
+                resultBuilder.append(childNodes.item(i).getNodeValue());
+            }
+            result = resultBuilder.toString();
+        }
+        return result;
+    }
+
 }
---
+++ src/uk/ac/ed/ph/snuggletex/utilities/UnwrappedParallelMathMLDOM.java
@@ -0,0 +1,49 @@
+/* $Id: org.eclipse.jdt.ui.prefs 3 2008-04-25 12:10:29Z davemckain $
+ *
+ * Copyright 2008 University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.snuggletex.utilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+/**
+ * Trivial wrapper Object that encapsulates the results of {@link MathMLUtilities#unwrapParallelMathMLDOM(Element)}.
+ *
+ * @author  David McKain
+ * @version $Revision: 3 $
+ */
+public class UnwrappedParallelMathMLDOM {
+    
+    private Element firstBranch;
+    private final Map<String, String> textAnnotations;
+    private final Map<String, NodeList> xmlAnnotaions;
+    
+    public UnwrappedParallelMathMLDOM() {
+        this.textAnnotations = new HashMap<String, String>();
+        this.xmlAnnotaions = new HashMap<String, NodeList>();
+    }
+
+    
+    public Element getFirstBranch() {
+        return firstBranch;
+    }
+    
+    public void setFirstBranch(Element firstBranch) {
+        this.firstBranch = firstBranch;
+    }
+
+    
+    public Map<String, String> getTextAnnotations() {
+        return textAnnotations;
+    }
+
+    
+    public Map<String, NodeList> getXmlAnnotaions() {
+        return xmlAnnotaions;
+    }
+}
