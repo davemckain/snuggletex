f9772d1b555cd606b652f792b5193b2f0456ac7c
David McKain <david.mckain@ed.ac.uk>
2009-01-21 10:43:19 +0000

    Added example of how to use the standalone up-conversion code to work on some ASCIIMathML.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@248 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

---
+++ src/uk/ac/ed/ph/snuggletex/extensions/samples/ASCIIMathMLUpConversionExample.java
@@ -0,0 +1,173 @@
+/* $Id$
+ *
+ * Copyright 2009 University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.snuggletex.extensions.samples;
+
+import uk.ac.ed.ph.snuggletex.DOMOutputOptions;
+import uk.ac.ed.ph.snuggletex.DOMPostProcessor;
+import uk.ac.ed.ph.snuggletex.SnuggleRuntimeException;
+import uk.ac.ed.ph.snuggletex.SnuggleEngine.DefaultStylesheetCache;
+import uk.ac.ed.ph.snuggletex.extensions.upconversion.MathMLUpConverter;
+import uk.ac.ed.ph.snuggletex.extensions.upconversion.UpConversionParameters;
+import uk.ac.ed.ph.snuggletex.extensions.upconversion.UpConvertingPostProcessor;
+import uk.ac.ed.ph.snuggletex.utilities.MathMLUtilities;
+import uk.ac.ed.ph.snuggletex.utilities.StylesheetCache;
+import uk.ac.ed.ph.snuggletex.utilities.UnwrappedParallelMathMLDOM;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * Simple demonstration of how to use some of the utilities within SnuggleTeX to up-convert
+ * some raw MathML extracted from ASCIIMathML.
+ * <p>
+ * You can do the same with the raw MathML produced by SnuggleTeX as well; you just have to
+ * call a {@link MathMLUpConverter#upConvertSnuggleTeXMathML(Document, Map)}) instead of the
+ * method for SnuggleTeX. When using SnuggleTeX normally, all of this can be invoked during
+ * the snuggling process by registering a {@link UpConvertingPostProcessor} with your
+ * {@link DOMOutputOptions#setDOMPostProcessor(DOMPostProcessor)}.
+ * 
+ * <h2>Running Notes</h2>
+ * 
+ * You will need the following in your ClassPath:
+ * 
+ * <ul>
+ *   <li>snuggletex.jar</li>
+ *   <li>saxon9.jar, saxon9-dom.jar</li> (These are required as the conversion process uses XSLT 2.0)
+ * </ul>
+ * 
+ * If you're already using XSLT 1.0 in your application and <strong>don't</strong> want your
+ * existing XSLT to run using SAXON (which is hard to justify, IMO, as SAXON is superior to other
+ * XSLT processors in most respects) then you might find that SAXON becomes your JAXP default
+ * processor, which you can override as you require as it is invoked here explicitly, rather than
+ * using {@link TransformerFactory#newInstance()}.
+ *
+ * @author  David McKain
+ * @version $Revision$
+ */
+public class ASCIIMathMLUpConversionExample {
+    
+    public static void main(String[] args) {
+        /* This is the MathML String you get from ASCIIMathML when you enter (5x)/(1-x).
+         * 
+         * NOTE: I'll have to check that we will always get proper UTF-8 from ASCIIMath so that
+         * we can store as a Java string without doing any further re-encoding work.
+         */
+        String asciiMathML = "<math title=\" (5x)/(1-x) \" xmlns=\"http://www.w3.org/1998/Math/MathML\">\n"
+            + "  <mstyle mathcolor=\"blue\" fontfamily=\"serif\" displaystyle=\"true\">\n" 
+            + "    <mfrac>\n" 
+            + "      <mrow>\n"
+            + "        <mn>5</mn>\n" 
+            + "        <mi>x</mi>\n" 
+            + "      </mrow>\n"
+            + "      <mrow>\n" 
+            + "        <mn>1</mn>\n" 
+            + "        <mo>-</mo>\n"
+            + "        <mi>x</mi>\n"
+            + "      </mrow>\n"
+            + "    </mfrac>\n" 
+            + "  </mstyle>\n" 
+            + "</math>";
+        
+        /* ============================================================================ */
+        /* To use the conversion stuff, we first need to parse this to get a DOM.
+         * This is all standard stuff.
+         * 
+         * Before doing this, we need to get a "DocumentBuilder" which is the JAXP
+         * handle on a DOM parser.
+         */
+        DocumentBuilder documentBuilder;
+        try {
+            documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        }
+        catch (ParserConfigurationException e) {
+            /* A failure here is going to be highly unlikely and fatal so is best thrown as some
+             * kind of unchecked Exception. I'll just use RuntimeException here.
+             */
+            throw new RuntimeException("Blah...", e);
+        }
+        
+        /* Do the actual parsing */
+        Document asciiMathMLDocument;
+        try {
+            asciiMathMLDocument = documentBuilder.parse(new InputSource(new StringReader(asciiMathML)));
+        }
+        catch (SAXException e) {
+            /* This would indicate bad MathML from ASCIIMath, which is a bug that would need fixed */
+            throw new RuntimeException("Blah...", e);
+        }
+        catch (IOException e) {
+            /* This shouldn't happen normally, unless we have an encoding issue */
+            throw new RuntimeException("Blah...", e);
+        }
+        
+        /* ============================================================================ */
+        /* Now we use some of the utilities I've put in SnuggleTeX to "up-convert" this. */
+        
+        /* Note 1: The conversion process is done with XSLT so it makes sense to cache compiled
+         * XSLT stylesheets for performance reasons. Hence, you're expected to create a cache
+         * and pass it to the up-converted. SnuggleTeX contains a default self-contained one
+         * that we'll use here. If you're using XSLT in your own application, you might want to
+         * integrate your own cache in using a suitable adapter. Any cache you make must be
+         * thread-safe.
+         * 
+         * In a servlet environment, you'd want the cache to be shared across the application.
+         * Exactly how you do that depends on how much of a framework you've got in place. At the
+         * lowest level, you can do it via the ServletContext.
+         */
+        StylesheetCache stylesheetCache = new DefaultStylesheetCache();
+        
+        /* Up-conversion is done by this utility class. It's thread-safe */
+        MathMLUpConverter upConverter = new MathMLUpConverter(stylesheetCache);
+        
+        /* You can control aspects of the conversion using a simple Map as follows.
+         * (Note: all of the things below are actually defaults but I've put them in for
+         * demo purposes. Have a look at UpConversionParameters for more possibilities.)
+         */
+        Map<String, Object> upconversionParameters = new HashMap<String, Object>();
+        upconversionParameters.put(UpConversionParameters.DO_CONTENT_MATHML, Boolean.TRUE);
+        upconversionParameters.put(UpConversionParameters.DO_MAXIMA, Boolean.TRUE);
+        
+        /* Now we do the up-conversion magic, which produces a new DOM Document. */
+        Document upconvertedDocument;
+        try {
+            upconvertedDocument = upConverter.upConvertASCIIMathML(asciiMathMLDocument, upconversionParameters);
+        }
+        catch (SnuggleRuntimeException e) {
+            /* This indicates a bug in the process so is currently notified via an unchecked
+             * Exception, so I'll just rethrow it here. You'll want this to be trickle up
+             * noisily so that the failure is logged properly.
+             */
+            throw e;
+        }
+        
+        /* ============================================================================ */
+        /* Demo of some utility methods for extracting results */
+        
+        /* 1. This is a convenience for serializing the resulting DOM Document back to an XML String */
+        String resultingMathMLString = MathMLUtilities.serializeDocument(upconvertedDocument);
+        System.out.println("Resulting MathML is:\n" + resultingMathMLString);
+        
+        /* 2. This demonstrates extracting a single annotation */
+        String maximaAnnotation = MathMLUtilities.extractAnnotationString(upconvertedDocument.getDocumentElement(), "Maxima");
+        System.out.println("Maxima Annotation was:\n" + maximaAnnotation);
+        
+        /* 3. Extracts all annotations into a wrapper Object */
+        UnwrappedParallelMathMLDOM unwrappedDOM = MathMLUtilities.unwrapParallelMathMLDOM(upconvertedDocument.getDocumentElement());
+        System.out.println("First branch of parallel MathML DOM was " + MathMLUtilities.serializeElement(unwrappedDOM.getFirstBranch()));
+        // Etc... 
+    }
+}
