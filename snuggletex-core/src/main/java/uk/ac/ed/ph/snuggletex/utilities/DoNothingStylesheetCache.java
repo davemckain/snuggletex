781d354af931139d437b4fb4464d721dbfcaedef
David McKain <david.mckain@ed.ac.uk>
2009-05-13 16:19:37 +0000

    Added DoNothingStylesheetCache, which might be convenient from time to time.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@321 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

---
+++ snuggletex-core/src/main/java/uk/ac/ed/ph/snuggletex/utilities/DoNothingStylesheetCache.java
@@ -0,0 +1,26 @@
+/* $Id$
+ *
+ * Copyright 2009 University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.snuggletex.utilities;
+
+import javax.xml.transform.Templates;
+
+/**
+ * Trivial implementation of {@link StylesheetCache} that actually doesn't cache anything
+ * at all. This might be useful during development when you don't want caching at all.
+ *
+ * @author  David McKain
+ * @version $Revision$
+ */
+public final class DoNothingStylesheetCache implements StylesheetCache {
+
+    public Templates getStylesheet(String resourceName) {
+        return null;
+    }
+    
+    public void putStylesheet(String resourceName, Templates stylesheet) {
+        /* (Do nothing) */
+    }
+}
