01bd04aafe28f3cbd542aac53ca09e22aabf3fef
David McKain <david.mckain@ed.ac.uk>
2009-05-26 16:58:15 +0000

    Fixed silly bug in new logic when applying multiple DOMPostProcessors
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@373 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

--- snuggletex-core/src/main/java/uk/ac/ed/ph/snuggletex/internal/DOMBuildingController.java
+++ snuggletex-core/src/main/java/uk/ac/ed/ph/snuggletex/internal/DOMBuildingController.java
@@ -39,7 +39,7 @@ public final class DOMBuildingController {
     public void buildDOMSubtree(final Element targetRoot, final List<FlowToken> fixedTokens)
             throws SnuggleParseException {
         DOMPostProcessor[] domPostProcessors = options.getDOMPostProcessors();
-        if (domPostProcessors!=null && domPostProcessors.length > 0) {
+        if (domPostProcessors!=null && domPostProcessors.length>0) {
             /* We run through each Post-Processor in turn, building a temporary "outputDocument"
              * and then using it as an input for the next stage. The final outputDocument is
              * used to decide what gets added into the target Document.
@@ -47,25 +47,27 @@ public final class DOMBuildingController {
             Document workDocument = XMLUtilities.createNSAwareDocumentBuilder().newDocument();
             Element workRoot = workDocument.createElementNS(SnuggleConstants.SNUGGLETEX_NAMESPACE, "root");
             workDocument.appendChild(workRoot);
+            
+            /* Do raw DOM Building */
+            DOMBuilder domBuilder = new DOMBuilder(sessionContext, workRoot, options);
+            domBuilder.buildDOMSubtree(fixedTokens);
+
+            /* Now let each post-processor mess around with the DOM */
             for (int processorIndex=0; processorIndex<domPostProcessors.length; processorIndex++) {
                 DOMPostProcessor domPostProcessor = domPostProcessors[processorIndex];
                 
-                /* Do raw DOM Building */
-                DOMBuilder domBuilder = new DOMBuilder(sessionContext, workRoot, options);
-                domBuilder.buildDOMSubtree(fixedTokens);
-                
                 /* Now let post-processor do its thing */
                 Document outputDocument = domPostProcessor.postProcessDOM(workDocument, options, sessionContext.getStylesheetManager());
-                Element adoptRoot;
                 
-                if (processorIndex==domPostProcessors.length - 1) {
+                Element adoptRoot;
+                if (processorIndex==domPostProcessors.length-1) {
                     /* This is the final step, so add to targetRoot */
                     adoptRoot = targetRoot;
                 }
                 else {
-                    /* Still go more steps to do, so we will be reusing workRoot. */
+                    /* Still go more steps to do, so we will be reusing workRoot.... */
                     adoptRoot = workRoot;
-                    /* But need to remove existing child Nodes first */
+                    /* ...but need to remove existing child Nodes first */
                     NodeList toDelete = workRoot.getChildNodes();
                     for (int i=toDelete.getLength()-1; i>=0; i--) {
                         workRoot.removeChild(toDelete.item(i));
@@ -75,7 +77,7 @@ public final class DOMBuildingController {
                 Element resultRoot = outputDocument.getDocumentElement();
                 NodeList childNodes = resultRoot.getChildNodes();
                 Node childNode;
-                for (int nodeIndex=0, size=childNodes.getLength(); nodeIndex<size; nodeIndex++) {
+                for (int i=0, size=childNodes.getLength(); i<size; i++) {
                     childNode = childNodes.item(0); /* (Adopting each child moves subsequent ones back!) */
                     adoptRoot.appendChild(adoptRoot.getOwnerDocument().adoptNode(childNode));
                 }
