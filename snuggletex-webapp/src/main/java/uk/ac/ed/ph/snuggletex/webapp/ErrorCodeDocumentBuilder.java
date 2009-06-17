6d035c2dc8673f9760c7bb0f7b5b38ff71159ea1
David McKain <david.mckain@ed.ac.uk>
2009-06-17 05:54:09 +0000

    Updated to split error codes into groups, and remove the now redundant extra title.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@417 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

--- snuggletex-webapp/src/main/java/uk/ac/ed/ph/snuggletex/webapp/ErrorCodeDocumentBuilder.java
+++ snuggletex-webapp/src/main/java/uk/ac/ed/ph/snuggletex/webapp/ErrorCodeDocumentBuilder.java
@@ -13,6 +13,8 @@ import uk.ac.ed.ph.snuggletex.utilities.SnuggleUtilities;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PropertyResourceBundle;
 
 /**
@@ -26,6 +28,23 @@ public final class ErrorCodeDocumentBuilder {
     
     private final File outputFile;
     
+    private static final Map<String, String> categoryHeadingMap;
+    
+    private static final String[] categoryData = {
+        "TTE", "Tokenisation Errors",
+        "TFE", "Token Fixing Errors",
+        "TDE", "DOM Building Errors",
+        "UCE", "Failures during Up-Conversion to Content MathML",
+        "UME", "Failures during Up-Conversion to Maxima syntax"
+    };
+    
+    static {
+        categoryHeadingMap = new HashMap<String, String>();
+        for (int i=0; i<categoryData.length; i+=2) {
+            categoryHeadingMap.put(categoryData[i], categoryData[i+1]);
+        }
+    }
+    
     public ErrorCodeDocumentBuilder(final File outputFile) {
         this.outputFile = outputFile;
     }
@@ -34,15 +53,25 @@ public final class ErrorCodeDocumentBuilder {
         IOUtilities.ensureFileCreated(outputFile);
         PrintWriter outputWriter = new PrintWriter(outputFile);
         outputWriter.println("\\pageId{errorCodes}");
-        outputWriter.println("\n\\section*{SnuggleTeX Error Codes}");
-        outputWriter.println("\n(In the table below, \\{0\\} et\\ al\\ are placeholders for details "
+        outputWriter.println("\n(In the tables below, \\{0\\} et\\ al\\ are placeholders for details "
                 + "specific to each error instance that are substituted in when formatting error messages)");
-        outputWriter.println("\n\\begin{tabular}{|c|l|}\n\\hline");
         
-        PropertyResourceBundle errorMessageBundle = MessageFormatter.ERROR_MESSAGE_BUNDLE;
+        String currentCategory = null;
+        String category;
         String codeName;
+        PropertyResourceBundle errorMessageBundle = MessageFormatter.ERROR_MESSAGE_BUNDLE;
         for (ErrorCode errorCode : ErrorCode.values()) {
             codeName = errorCode.name();
+            category = codeName.substring(0,3);
+            if (!category.equals(currentCategory)) {
+                /* Starting new category; finish current table if open */
+                if (currentCategory!=null) {
+                    outputWriter.println("\\end{tabular}");                    
+                }
+                currentCategory = category;
+                outputWriter.println("\n\\subsection*{" + category + ": " + categoryHeadingMap.get(category));
+                outputWriter.println("\\begin{tabular}{|c|l|}\n\\hline");
+            }
             outputWriter.println("\\anchor{"
                     + codeName
                     + "}\\verb|"
@@ -51,7 +80,9 @@ public final class ErrorCodeDocumentBuilder {
                     + SnuggleUtilities.quoteTextForInput(errorMessageBundle.getString(codeName))
                     + " \\\\ \\hline");
         }
-        outputWriter.println("\\end{tabular}");
+        if (currentCategory!=null) {
+            outputWriter.println("\\end{tabular}");
+        }
         outputWriter.close();
     }
     
