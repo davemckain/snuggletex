f179eba2861d350a17dc4366db7ec530d8307cc3
David McKain <david.mckain@ed.ac.uk>
2009-10-30 16:44:39 +0000

    Added filter to redirect certain URLs according to prescribed patterns. This will allow me to move documentation around a bit without breaking links.
    git-svn-id: https://svn.code.sf.net/p/snuggletex/code/trunk@484 cfd59bc9-af4b-0410-a4ae-d9b3a5cde016

---
+++ snuggletex-webapp/src/main/java/uk/ac/ed/ph/snuggletex/webapp/RedirectionFilter.java
@@ -0,0 +1,74 @@
+/* $Id$
+ *
+ * Copyright 2009 University of Edinburgh.
+ * All Rights Reserved
+ */
+package uk.ac.ed.ph.snuggletex.webapp;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple little {@link Filter} that allows us to apply redirections (specified in web.xml)
+ * to cater for the case where pages move about or get better names.
+ *
+ * @author  David McKain
+ * @version $Revision$
+ */
+public final class RedirectionFilter implements Filter {
+    
+    private static final Logger logger = LoggerFactory.getLogger(RedirectionFilter.class);
+    
+    private final LinkedHashMap<Pattern, String> redirectionMap = new LinkedHashMap<Pattern, String>();
+    
+    public void init(FilterConfig filterConfig) {
+        String redirectionData = filterConfig.getInitParameter("redirections");
+        if (redirectionData!=null) {
+            redirectionMap.clear();
+            String[] fields = redirectionData.split("\\s+");
+            Pattern pattern;
+            String target;
+            for (int i=0; i<fields.length; ) {
+                pattern = Pattern.compile(fields[i++]);
+                target = fields[i++];
+                logger.info("Registering redirect " + pattern + " => " + target);
+                redirectionMap.put(pattern, target);
+            }
+        }
+    }
+    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        
+        /* See if within-context URL matches one of our patterns. If so, redirect */
+        String requestUrl = WebUtilities.getWithinContextRequestUrl(httpRequest);
+        for (Entry<Pattern, String> mapEntry : redirectionMap.entrySet()) {
+            if (mapEntry.getKey().matcher(requestUrl).matches()) {
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.sendRedirect(httpRequest.getContextPath() + mapEntry.getValue());
+                return;
+            }
+        }
+        /* If still here, then let request proceed as normal */
+        filterChain.doFilter(request, response);
+    }
+    
+    public void destroy() {
+        /* (Nothing to do) */
+    }
+}
